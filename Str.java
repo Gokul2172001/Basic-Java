a19bedb9c10dff046f6bd565af65c3b8f9df4d7f
Gokul2172001 <76160646+Gokul2172001@users.noreply.github.com>
2022-03-15 22:53:56 +0530

    Add files via upload
    Commited.

---
+++ Str.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Str {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		String t=input.nextLine();
+		String tr=input.nextLine();
+		//int a=input.nextInt();
+		//int b=input.nextInt();
+				//System.out.println(t==tr);
+				//System.out.println((t).compareTo(tr));
+				//System.out.println((t).equals (tr));
+		//System.out.println(t.concat (tr));
+		//System.out.println(10+20+t+10+10+tr);
+		//System.out.println(a+t+b+a+tr);
+		//System.out.println(t+tr);
+		//System.out.println((t).endsWith("s"));
+		//System.out.println((tr).endsWith("t"));
+		//System.out.println((t).startsWith(tr));
+		//System.out.println((tr).startsWith("t"));
+		//System.out.println((t).equalsIgnoreCase(tr));
+		//System.out.println((t).indexOf(tr));
+		//System.out.println((t).trim()+(tr));
+		//System.out.println(t.length());
+		//tr=t.replace('e','o');
+		//tr=t.toLowerCase();
+		tr=t.toUpperCase();
+		System.out.println(tr);
+		
+	}
+
+}
