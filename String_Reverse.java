e01dc02693d9b67e0e0d0c0de26865b20a957f33
Gokul2172001 <76160646+Gokul2172001@users.noreply.github.com>
2022-03-17 22:52:35 +0530

    Add files via upload
    Commited.

---
+++ String_Reverse.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class String_Reverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		String s=input.nextLine();
+		StringBuffer str=new StringBuffer(s);
+		str.reverse();
+		System.out.print(str);
+		
+
+	}
+
+}
