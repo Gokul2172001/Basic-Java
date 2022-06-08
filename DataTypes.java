e2a6a3730e82a2a57d65b0367bb4c0f49ae3af88
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2022-06-08 23:21:13 +0530

    Commited.

---
+++ DataTypes.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		Scanner input1=new Scanner(System.in);
+		int i=input.nextInt();
+		double d=input.nextDouble();
+		String s=input1.nextLine();
+		System.out.println("Interger: "+i);
+		System.out.println("Floating: "+d);
+		System.out.println("String: "+s);
+
+	}
+
+}
---
+++ SkinAndFlesh.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class SkinAndFlesh {
+	public static Scanner input;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		input=new Scanner(System.in);
+		int a=input.nextInt();
+		int skin=1,flesh=0;
+		int arr[][]=new int[a][a];
+		for(int i=0;i<a;i++)
+		{
+			for(int j=0;j<a;j++)
+			{
+				System.out.print(skin);
+			}
+			System.out.println();
+		}
+		 
+	}
+
+}
