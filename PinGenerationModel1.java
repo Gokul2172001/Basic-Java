d380dc8dc2b207555bcffa0d83089e40474e950e
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2022-06-21 21:26:10 +0530

    Commited.

--- PinGenerationModel1.java
+++ PinGenerationModel1.java
@@ -10,8 +10,12 @@ public class PinGenerationModel1 {
 			String s2=String.valueOf(inp2);
 			String s3=String.valueOf(inp3);
 			String s= s1+s2+s3;
-			System.out.println(s);
+			//System.out.println(s1);
+			//System.out.println(s2);
+			//System.out.println(s3);
 			int arr[]=new int[s.length()];
+			//int j=Arrays.toString(arr);
+			System.out.println(s);			
 			for(int i=0;i<s.length();i++) {
 				arr[i]=s.charAt(i);
 				if(arr[i]%2==0) {
