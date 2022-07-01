5a1c48c228acfde8835ce30051c6c4ac59d65d37
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2022-07-01 22:08:11 +0530

    Committed.

--- PrimeNumber_Between_Range.java
+++ PrimeNumber_Between_Range.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 public class PrimeNumber_Between_Range {
 	public static Scanner input;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		input=new Scanner(System.in);
 		int count=0;
 		int num1=input.nextInt();
 		int num2=input.nextInt();
 		for(int i=num1;i<num2;i++) {
 			for(int j=2;j<=i;j++) {
-				if(num1%j==0) {
+				if(num1%j==0)
+				{
 					count=count+1;
 				}
 			}
