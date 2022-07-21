349a5c529482a9906cc4526b82a14bd82d87e2bc
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2022-07-21 22:37:28 +0530

    Committed.

---
+++ PinGeneration4.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author ELCOT
+ *
+ */
+public class PinGeneration4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
