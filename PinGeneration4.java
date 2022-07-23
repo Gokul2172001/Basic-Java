2da8b5873285cdc5f27a3c383bbf98f90173e205
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2022-07-23 21:38:04 +0530

    Committed.

--- PinGeneration4.java
+++ PinGeneration4.java
@@ -3,6 +3,10 @@ public class PinGeneration4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		String[] inp1=input.PasrseInt();
+		String[] inp1=input.PasrseInt();
+		String[] inp1=input.PasrseInt();
 
 	}
 
