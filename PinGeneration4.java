3ed8f2a6df5392000d2b2883af613aed367b24bf
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2022-07-26 22:28:34 +0530

    Committed.

--- PinGeneration4.java
+++ PinGeneration4.java
@@ -4,8 +4,8 @@ public class PinGeneration4 {
 	public static void main(String[] args) {
 		Scanner input=new Scanner(System.in);
 		String[] inp1=input.PasrseInt();
-		String[] inp1=input.PasrseInt();
-		String[] inp1=input.PasrseInt();
+		String[] inp2=input.PasrseInt();
+		String[] inp3=input.PasrseInt();
 
 	}
 
