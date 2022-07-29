9f06f5b96aeb3cb3d6bd5fd3b9002c70637fc2c3
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2022-07-29 22:57:29 +0530

    Committed.

--- PinGeneration4.java
+++ PinGeneration4.java
@@ -7,7 +7,10 @@ public class PinGeneration4 {
 		String[] inp2=input.PasrseInt();
 		String Builder inp3=inp1.trim(inp2);
 		String Builder inp4=toUppercase(inp3);
+		for(int i=0;i<length.inp1();i++) {
 		String arr[]=inp1.charAt();
+		}
+			
 
 	}
 
